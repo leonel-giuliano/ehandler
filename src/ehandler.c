a3afa1e0998e356141b8188bb071003c0a2fdd00
leonel-giuliano <leonelgiuliano7@gmail.com>
2025-11-16 10:47:53 -0300

    Added function to set option error message

---
+++ src/ehandler.c
@@ -0,0 +1,20 @@
+#include "ehandler.h"
+
+
+#ifdef USE_OPEMSG
+
+
+#include <string.h>
+
+
+static struct _opemsg _opemsg = { 0 };
+
+
+void __set_opemsg(size_t line, const char *file, const char *msg) {
+	_opemsg.line = line;
+	strncpy(_opemsg.file, file, ABS_PATH_MAX);
+	strncpy(_opemsg.msg, msg, __OPEMSGSIZ);
+}
+
+
+#endif
