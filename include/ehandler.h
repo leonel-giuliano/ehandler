8bd5cda9aea6f891b170498f3cbf044d40f12e98
leonel-giuliano <leonelgiuliano7@gmail.com>
2025-11-16 10:37:02 -0300

    Added struct for optional error messages

--- include/ehandler.h
+++ include/ehandler.h
@@ -7,6 +7,28 @@
 #include <errno.h>
 
 
+#ifdef __linux__
+#include <linux/limits.h>
+#define ABS_PATH_MAX PATH_MAX
+#elif __unix__
+#include <limits.h>
+#define ABS_PATH_MAX ABS_PATH_MAX
+#elif _WIN32
+#include <windows.h>
+#define ABS_PATH_MAX MAX_PATH
+#endif
+
+
+#define __OPEMSGSIZ 64
+
+
+struct _opemsg {
+	size_t 	line;
+	char 	file[ABS_PATH_MAX];
+	char 	msg[__OPEMSGSIZ];
+};
+
+
 #define EMSG(_Msg) do {																\
 	int __errno_val = errno;														\
 	fprintf(stderr, "\n(%s:%d) ERROR: %s", __FILE__, __LINE__, _Msg);				\
