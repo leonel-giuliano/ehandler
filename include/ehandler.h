ce4cb93a79977964427deb0641f8392e7f74285c
leonel-giuliano <leonelgiuliano7@gmail.com>
2025-11-16 10:21:24 -0300

    Added function to print error message

---
+++ include/ehandler.h
@@ -0,0 +1,18 @@
+#ifndef __LIBEHANDLER_H__
+
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+
+#define EMSG(_Msg) do {																\
+	int __errno_val = errno;														\
+	fprintf(stderr, "\n(%s:%d) ERROR: %s", __FILE__, __LINE__, _Msg);				\
+	if(__errno_val > 0)																\
+		fprintf(stderr, " | %s [ERRNO::%d]", strerror(__errno_val), __errno_val);	\
+	putc('\n', stderr);																\
+} while(0)
+
+
+#endif // !__LIBEHANDLER_H__
